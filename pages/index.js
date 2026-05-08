a79348c9f487ddaf1d1b278fd74e32922d49a52f
Kornelije Vizin <126393918+Kornelije-Vizin@users.noreply.github.com>
2026-05-08 19:36:56 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,42 +2,46 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div>
-      <div className="login-user">
-        <div className="login-user__top">
-          <div className="login-user__logo" />
+    <main className="login-user">
+      <div className="login-user__top">
+        <div className="login-user__logo" />
 
-          <span className="login-user__title">
-            Who Is Watching?
-          </span>
-        </div>
+        <span className="login-user__title">
+          Who Is Watching?
+        </span>
+      </div>
 
-        <div className="login-user__form">
-          <div className="login-user__user-box">
-            <Image
-              className="login-user__user-img"
-              src="https://mighty.tools/mockmind-api/content/human/39.jpg"
-              alt="Random human face"
-              width={150}
-              height={150}
-            />
+      <div className="login-user__form">
+        <div className="login-user__user-box">
+          <Image
+            className="login-user__user-img"
+            src="https://mighty.tools/mockmind-api/content/human/39.jpg"
+            alt="Bryant profile"
+            width={150}
+            height={150}
+          />
 
-            <div className="login-user__user-name">
-              Bryant
-            </div>
+          <div className="login-user__user-name">
+            Bryant
           </div>
         </div>
-
-        <div className="login-user__buttons">
-          <button className="login-user__adult">
-            Add Adult
-          </button>
-
-          <button className="login-user__kid">
-            Add Kid
-          </button>
-        </div>
       </div>
-    </div>
+
+      <div className="login-user__buttons">
+        <button
+          type="button"
+          className="login-user__adult"
+        >
+          Add Adult
+        </button>
+
+        <button
+          type="button"
+          className="login-user__kid"
+        >
+          Add Kid
+        </button>
+      </div>
+    </main>
   );
 }
