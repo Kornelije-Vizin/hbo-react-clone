d5dce8200f2645c69b2d506325839c2510882799
Kornelije Vizin <126393918+Kornelije-Vizin@users.noreply.github.com>
2026-05-08 20:07:31 +0200

    Update CastInfo.js

--- components/UI/CastInfo/CastInfo.js
+++ components/UI/CastInfo/CastInfo.js
@@ -1,70 +1,49 @@
-import Image from 'next/image';
-
-
-const CastInfo = (props) => {
-  return(
+const CastInfo = () => {
+  return (
     <div className="cast-info">
       <div className="cast-info__group-title">
         Cast & Crew
       </div>
+
       <div className="cast-info__list">
         <ul className="cast-info__crew">
-          <li>
-            James
-          </li>
-          <li>
-            George Lucas
-          </li>
+          <li>James</li>
+          <li>George Lucas</li>
         </ul>
+
         <ul className="cast-info__crew">
-          <li>
-            Billy
-          </li>
-          <li>
-            George Lucas
-          </li>
+          <li>Billy</li>
+          <li>George Lucas</li>
         </ul>
+
         <ul className="cast-info__crew">
-          <li>
-            Liu Kang
-          </li>
-          <li>
-            George Lucas
-          </li>
+          <li>Liu Kang</li>
+          <li>George Lucas</li>
         </ul>
+
         <ul className="cast-info__crew">
-          <li>
-            Raul
-          </li>
-          <li>
-            George Lucas
-          </li>
+          <li>Raul</li>
+          <li>George Lucas</li>
         </ul>
+
         <ul className="cast-info__crew">
-          <li>
-            Samantha
-          </li>
-          <li>
-            George Lucas
-          </li>
+          <li>Samantha</li>
+          <li>George Lucas</li>
         </ul>
       </div>
+
       <div className="cast-info__group-title">
         Director
       </div>
+
       <div className="cast-info__list">
         <ul className="cast-info__crew">
-          <li>
-            James
-          </li>
-          <li>
-            George Lucas
-          </li>
+          <li>James</li>
+          <li>George Lucas</li>
         </ul>
-        
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default CastInfo;
