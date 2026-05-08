4ca4d03aad91fa1432daef4cb5437b059579e9c9
Kornelije Vizin <126393918+Kornelije-Vizin@users.noreply.github.com>
2026-05-08 19:31:25 +0200

    Update movie.js

--- pages/movie.js
+++ pages/movie.js
@@ -1,10 +1,7 @@
-import Head from 'next/head'
-import MainLayout from '@/components/Layouts/MainLayout'
-import CastInfo from '../components/UI/CastInfo/CastInfo'
-import FeaturedMedia from '../components/UI/FeaturedMedia/FeaturedMedia'
-import ForYouList from '../components/UI/ForYouList/ForYouList'
-import JustAdded from '../components/UI/JustAdded/JustAdded'
-import PosterView from '../components/UI/PosterView/PosterView'
+import MainLayout from '@/components/Layouts/MainLayout';
+import CastInfo from '@/components/UI/CastInfo/CastInfo';
+import FeaturedMedia from '@/components/UI/FeaturedMedia/FeaturedMedia';
+import PosterView from '@/components/UI/PosterView/PosterView';
 
 export default function HomeView() {
   return (
@@ -13,5 +10,5 @@ export default function HomeView() {
       <PosterView />
       <CastInfo />
     </MainLayout>
-  )
+  );
 }
