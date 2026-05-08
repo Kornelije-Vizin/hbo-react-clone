2294f32b5fe8211cdd109045da74f4c606467c7f
Kornelije Vizin <126393918+Kornelije-Vizin@users.noreply.github.com>
2026-05-08 19:19:47 +0200

    Update Account.js

--- components/UI/Account/Account.js
+++ components/UI/Account/Account.js
@@ -1,44 +1,64 @@
 import Link from 'next/link';
 import Image from 'next/image';
 
-
-const Account = (props) => {
+const Account = () => {
   const loopComp = (comp, digit) => {
     let thumbnails = [];
-    for(let index = 1; index <= digit; index++) {
-      thumbnails.push(comp)
+
+    for (let index = 1; index <= digit; index++) {
+      thumbnails.push(
+        <div key={index}>
+          {comp}
+        </div>
+      );
     }
 
     return thumbnails;
-  }
-  return(
+  };
+
+  return (
     <div className="account">
       <div className="account__details">
         <div className="account__title">My List</div>
+
         <div className="account__watch-list">
-          {loopComp((<div className="account__watch-video">
-          <Image src="https://cdn.shopify.com/s/files/1/0013/2874/2466/products/rick-and-morty-tv-invasion-poster-24-x-36-581_1024x.jpg?v=1616627934" alt="Rick and Morty TV Invasion Poster" />
+          {loopComp(
+            <div className="account__watch-video">
+              <Image
+                src="https://cdn.shopify.com/s/files/1/0013/2874/2466/products/rick-and-morty-tv-invasion-poster-24-x-36-581_1024x.jpg?v=1616627934"
+                alt="Rick and Morty TV Invasion Poster"
+                width={300}
+                height={450}
+              />
+
               <div className="account__watch-overlay">
-              <div className="account__watch-buttons">
-                <div className="account__watch-circle">
-                  <i className="fas fa-play"/>
-                </div>
-                <div className="account__watch-circle">
-                  <i className="fas fa-times"/>
+                <div className="account__watch-buttons">
+                  <div className="account__watch-circle">
+                    <i className="fas fa-play" />
+                  </div>
+
+                  <div className="account__watch-circle">
+                    <i className="fas fa-times" />
+                  </div>
                 </div>
               </div>
-            </div>
-          </div>), 6)}
-          
+            </div>,
+            6
+          )}
         </div>
       </div>
+
       <div className="account__menu">
         <ul className="account__main">
           <li>
-            <Link href="/" className="active">My List</Link>
+            <Link href="/" className="active">
+              My List
+            </Link>
           </li>
         </ul>
+
         <div className="side-nav__divider" />
+
         <ul className="account__main">
           <li>
             <Link href="/">Account</Link>
@@ -49,7 +69,7 @@ const Account = (props) => {
         </ul>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Account;
