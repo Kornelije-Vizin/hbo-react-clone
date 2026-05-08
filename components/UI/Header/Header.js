a63f1eab06c0b67ebc14cbec9db2bde7bfa59359
Kornelije Vizin <126393918+Kornelije-Vizin@users.noreply.github.com>
2026-05-08 19:21:47 +0200

    Update Header.js

--- components/UI/Header/Header.js
+++ components/UI/Header/Header.js
@@ -1,27 +1,38 @@
-import Account from "../Account/Account"
-import SearchModal from "../SearchModal/SearchModal"
-import Image from 'next/image';
+import Account from "../Account/Account";
+import SearchModal from "../SearchModal/SearchModal";
+import Image from "next/image";
 
+const Header = () => {
+  return (
+    <header className="top-header">
+      <div className="top-header__left-side">
+        <div className="top-header__menu-btn">
+          <i className="fas fa-bars" />
+        </div>
 
-
-const Header = (props) => {
-  return(<header className="top-header">
-    <div className="top-header__left-side">
-      <div className="top-header__menu-btn">
-        <i className="fas fa-bars"/>
+        <div className="top-header__search-btn">
+          <i className="fas fa-search" />
+        </div>
       </div>
-      <div className="top-header__search-btn">
-        <i className="fas fa-search"/>
+
+      <div className="top-header__logo"></div>
+
+      <div className="top-header__account">
+        <Image
+          src="https://uifaces.co/our-content/donated/vIqzOHXj.jpg"
+          className="top-header__user-img"
+          alt="User Profile"
+          width={40}
+          height={40}
+        />
+
+        <div className="top-header__user-name">Bryant</div>
       </div>
-    </div>
-    <div className="top-header__logo"></div>
-    <div className="top-header__account">
-    <Image src="https://uifaces.co/our-content/donated/vIqzOHXj.jpg" className="top-header__user-img" alt="User Profile"/>
-      <div className="top-header__user-name">Bryant</div>
-    </div>
-    <Account />
-    <SearchModal />
-  </header>)
-}
+
+      <Account />
+      <SearchModal />
+    </header>
+  );
+};
 
 export default Header;
