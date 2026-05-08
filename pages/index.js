3714e927eab9a1ccdabc5619599055921dbdb6aa
Kornelije Vizin <126393918+Kornelije-Vizin@users.noreply.github.com>
2026-05-08 19:32:03 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,12 @@
-import Head from 'next/head'
 import Image from 'next/image';
-
-
 
 export default function Home() {
   return (
     <div>
       <div className="login-user">
         <div className="login-user__top">
-          <div className="login-user__logo"/>
+          <div className="login-user__logo" />
+
           <span className="login-user__title">
             Who Is Watching?
           </span>
@@ -16,15 +14,30 @@ export default function Home() {
 
         <div className="login-user__form">
           <div className="login-user__user-box">
-            <Image className="login-user__user-img" src="https://mighty.tools/mockmind-api/content/human/39.jpg" alt="Random human face" />
-            <div className="login-user__user-name">Bryant</div>
+            <Image
+              className="login-user__user-img"
+              src="https://mighty.tools/mockmind-api/content/human/39.jpg"
+              alt="Random human face"
+              width={150}
+              height={150}
+            />
+
+            <div className="login-user__user-name">
+              Bryant
+            </div>
           </div>
         </div>
+
         <div className="login-user__buttons">
-          <button className="login-user__adult">Add Adult</button>
-          <button className="login-user__kid">Add Kid</button>
+          <button className="login-user__adult">
+            Add Adult
+          </button>
+
+          <button className="login-user__kid">
+            Add Kid
+          </button>
         </div>
       </div>
     </div>
-  )
+  );
 }
